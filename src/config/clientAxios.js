018157f7402b2f63c02e3b81bba84ccb2637f82c
Tapioka <tapioka@pop-os.localdomain>
2022-06-24 20:03:15 -0500

    https arreglado

--- src/config/clientAxios.js
+++ src/config/clientAxios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const clientAxios = axios.create({
-  baseURL: "http://yamichan.us-3.evennode.com/",
-//baseURL:"http://localhost:4000"
+  baseURL: "https://yamichan.us-3.evennode.com/",
+  //baseURL:"http://localhost:4000"
 });
